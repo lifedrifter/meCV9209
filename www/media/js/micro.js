59a762bb6cde4945420887433dd992c04198f690
demcoder <doc.viktor@gmail.com>
2018-08-15 21:55:37 +0200

    images

--- www/media/js/micro.js
+++ www/media/js/micro.js
@@ -2,3 +2,4 @@
 $().ready(function(){
 	
 });
+$(".content").attr('unselectable','on').css({'-moz-user-select':'-moz-none','-moz-user-select':'none','-o-user-select':'none','-khtml-user-select':'none','-webkit-user-select':'none','-ms-user-select':'none','user-select':'none'}).bind('selectstart', function(){ return false; });
